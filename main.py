dddc798996bbd536965063607f417983a397dd46
макар <130247171+banikhlh@users.noreply.github.com>
2024-07-30 17:23:42 +0300

    email

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import string
 from fastapi import FastAPI, HTTPException, Response
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+import re
+
+
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
 app = FastAPI()
 
@@ -23,7 +27,8 @@ def create_tables():
         """CREATE TABLE IF NOT EXISTS name_table (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     username_db TEXT UNIQUE,
-                    password_db TEXT
+                    password_db TEXT,
+                    email TEXT UNIQUE
                 )"""
     )
     conn.commit()
@@ -42,16 +47,26 @@ def user_exists(username: str) -> bool:
     return exists
 
 
-def reg(username: str, password: str):
+def reg(username: str, password: str, email: str):
     if user_exists(username):
         raise HTTPException(status_code=400, detail="Username already exists")
+    else:
+        if re.fullmatch(regex, email):
+            conn = get_db_connection()
+            c = conn.cursor()
+            c.execute('SELECT 1 FROM name_table WHERE email = ?', (email,))
+            email1 = c.fetchone() is not None
+            if email1:
+                raise HTTPException(status_code=400, detail="Email already exists")
+        else:
+            raise HTTPException(status_code=401, detail="Invalid credentials")
 
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     conn = get_db_connection()
     c = conn.cursor()
     c.execute(
-        "INSERT INTO name_table (username_db, password_db) VALUES (?, ?)",
-        (username, hashed_password),
+        "INSERT INTO name_table (username_db, password_db, email) VALUES (?, ?, ?)",
+        (username, hashed_password, email),
     )
     conn.commit()
     conn.close()
@@ -64,6 +79,7 @@ def generate_session_token(length: int) -> str:
 class UserCreate(BaseModel):
     username: str
     password: str
+    email: str
 
 
 @app.post("/login")
@@ -71,10 +87,7 @@ async def login(user: UserCreate, response: Response):
     hashed_password = hashlib.sha256(user.password.encode()).hexdigest()
     conn = get_db_connection()
     c = conn.cursor()
-    c.execute(
-        "SELECT * FROM name_table WHERE username_db = ? AND password_db = ?",
-        (user.username, hashed_password),
-    )
+    c.execute('SELECT * FROM name_table WHERE username_db = ? AND password_db = ? AND email = ?', (user.username, hashed_password, user.email))
     user_row = c.fetchone()
     conn.close()
 
@@ -90,8 +103,8 @@ async def login(user: UserCreate, response: Response):
 
 @app.post("/register")
 async def register(user: UserCreate):
-    reg(user.username, user.password)
-    return JSONResponse(content={"message": "User registered successfully"})
+    reg(user.username, user.password, user.email)
+    return JSONResponse(content={"message": "User registered successfull"})
 
 
 if __name__ == "__main__":
